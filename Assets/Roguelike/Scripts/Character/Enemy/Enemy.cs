420d8f80bda7e6cddbe7ccd1ca3b9d4022b813ce
viviria <vivivi.viviria@gmail.com>
2020-04-05 01:41:30 +0900

    敵キャラクタを仮配置．

---
+++ Assets/Roguelike/Scripts/Character/Enemy/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Roguelike {
+  public class Enemy : Character
+  {
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+      base.Start();
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+      base.Update();
+    }
+  }
+}
